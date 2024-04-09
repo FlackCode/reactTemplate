ca0be1f43591761ae48a4c377b9518e0018d42d0
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-09 19:30:54 +0200

    Updated Tailwind config with an extra screen setting.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,18 @@ export default {
   content: [
     "./index.html",
     "./src/*.{js,ts,jsx,tsx}",
+    "./src/components/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    screens: {
+      'xsm' : '300px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
