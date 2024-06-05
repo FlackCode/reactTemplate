f782a40f6cf953bb6ed282bdf4c1056b32fe251d
flack <163582560+FlackCode@users.noreply.github.com>
2024-06-05 08:07:33 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ export default {
     "./src/components/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        glow: [
+          "0 0 20px rgba(0, 0, 255, 0.6)",
+          "0 0px 65px rgba(0, 0,255, 0.2)"
+        ]
+      },
+      colors: {
+        'background': '#080808',
+        'text': '#f7f7f7',
+        'highlight': '#60a5fa',
+        'primary': '#3b82f6',
+        'hover': '#2563eb'
+      }
+    },
     screens: {
       'xsm' : '300px',
       'sm': '640px',
@@ -14,8 +28,8 @@ export default {
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
-    }
+    },
+    
   },
   plugins: [],
 }
-
